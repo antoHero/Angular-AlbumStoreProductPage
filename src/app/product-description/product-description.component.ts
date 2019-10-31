ecdf050163997ddb90bbe014fc3ab077b34f96e1
antoHero <veeqanto@gmail.com>
2019-11-01 00:09:45 +0100

    AlbumStore v1.1

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductService} from '../product.service';
+import { AlbumStorePage } from '../../../e2e/app.po';
 
 @Component({
   selector: 'app-product-description',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
+  albumInfo;
 
-  constructor() { }
+  constructor(private _productservice: ProductService) { }
 
   ngOnInit() {
+    this._productservice.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
 
 }
--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductPageComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
